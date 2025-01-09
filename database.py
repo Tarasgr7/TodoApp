1139cc7fff5292840c21661a23dbec2d454d37d1
Tarasgr7 <tarashrickyv@gmail.com>
2025-01-09 16:23:54 +0200

    Change dabase

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todo_postgre_sql_5oqk_user:yAz6bMxcDqCLx6fyrjW0r0FIEeomAdN6@dpg-ctvtn1rv2p9s7399osrg-a/todo_postgre_sql_5oqk'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
