a18464cdf4a86688183ec50f9994ac3de487eca9
Tarasgr7 <tarashrickyv@gmail.com>
2025-01-09 16:29:37 +0200

    Update change database

--- database.py
+++ database.py
@@ -4,7 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://todo_postgre_sql_5oqk_user:yAz6bMxcDqCLx6fyrjW0r0FIEeomAdN6@dpg-ctvtn1rv2p9s7399osrg-a/todo_postgre_sql_5oqk'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
